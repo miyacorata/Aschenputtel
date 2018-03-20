a53470cf4a94db7192865b4d44699c571e538c76
miyacorata <miyano@cg-portal.net>
2018-03-20 19:28:35 +0900

    注入するCSSの変更

--- preload.js
+++ preload.js
@@ -1,5 +1,12 @@
 var style = document.createElement('style');
-style.textContent = 'body::-webkit-scrollbar{display: none;}';
+style.textContent = `
+*::-webkit-scrollbar{
+    display: none;
+}
+.area-slide{
+    overflow-y:scroll!important;
+}
+`;
 
 document.addEventListener("DOMContentLoaded", () => {
     document.head.appendChild(style);
